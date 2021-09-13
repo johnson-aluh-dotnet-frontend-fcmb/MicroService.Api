710b649621d574b9dfa84c75cb2861a91b8c0d32
AluhJohnson <johnsonaluh@gmail.com>
2021-09-13 22:08:51 +0100

    Added ProductContext class

---
+++ Product/ProductApi/DbContexts/ProductContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductApi.Model;
+
+namespace ProductApi.DbContexts
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    Id = 1,
+                    Name = "Electronics"
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Computer"
+                },
+                new Category
+                {
+                    Id = 3,
+                    Name = "Clothings"
+                },
+                new Category
+                {
+                    Id = 4,
+                    Name = "Grocery"
+                });
+        }
+        DbSet<Product> Products { get; set; }
+        DbSet<Category> Categories { get; set; }
+    }
+}
