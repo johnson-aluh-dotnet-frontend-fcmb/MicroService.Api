3d2425512a5dae4f9c7f210d7ae0cee38482a1ef
AluhJohnson <johnsonaluh@gmail.com>
2021-09-13 21:49:58 +0100

    Added Product model with Id, name, Description and Price

---
+++ Product/ProductApi/Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductApi.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Decription { get; set; }
+        public int Price { get; set; }
+    }
+}
